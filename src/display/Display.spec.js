e3f53868b7f5e6b937609cd77d3dfffcd4e29f81
Amy Barba <Amybarba70@gmail.com>
2019-10-01 20:42:37 -0700

    display.spec.js first attempt

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+import React from 'react';
+import * as rtl from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import Display from './Display';
+afterEach(rtl.cleanup);
+
+describe('Display', () => {
+    it('renders correctly', () => {
+        const displayWrapper = rtl.render(<Display />);
+        expect(displayWrapper.baseElement)
+        .toMatchSnapshot();
+    });
+});
